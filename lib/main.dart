f8523c2191659b4607435e5f21a0d361441288d4
snehau259 <snehau259@gmail.com>
2023-03-01 20:02:07 +0530

    add. appBar

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:js';
+
+import 'package:blood_donation_app/pages/home.dart';
+import 'package:flutter/material.dart';
+// import 'package:my_first_flutter_app/widgets/container.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); //constructor
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      title: "Demo App",
+      home: Home(),
+      routes: {'/':(context)=>Home()},
+      initialRoute: '/',
+    );
+  }
+}
