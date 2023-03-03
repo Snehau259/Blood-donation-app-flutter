4145bdc6abb708fa960716f40aff64de3178914a
snehau259 <snehau259@gmail.com>
2023-03-03 09:58:02 +0530

    refactor. remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-// import 'dart:js';
-
+import 'dart:js';
 import 'package:blood_donation_app/pages/ListDonors.dart';
 import 'package:blood_donation_app/pages/addDonor.dart';
 import 'package:blood_donation_app/pages/home.dart';
@@ -7,9 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-// import 'package:my_first_flutter_app/widgets/container.dart';
 
 Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
